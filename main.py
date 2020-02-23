faf4108165d995d43f6c7bf0851e65c63fea0017
l0rb <lorbritzer@yahoo.de>
2020-02-23 22:58:51 +0100

    augment service to spew out some data, expirementally

--- main.py
+++ main.py
@@ -5,8 +5,16 @@ from tinyserver import miniServer, miniRequestHandler
 
 csms = CSMS()
 
+def do_data():
+    from db import DBConnection
+    from schema import Point
+    conn = DBConnection()
+    return json.dumps({conn.session.query(Point).all()})
+
 class csmsRequestHandler(miniRequestHandler):
     def message(self):
+        if self.path == '/data':
+            return do_data()
         return json.dumps({
             'relative': csms.relative
         })
